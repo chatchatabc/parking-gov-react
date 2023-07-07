fdc199716a60c331a390e13847843ad3b013a78e
BoJoNVi <bonjomontes@gmail.com>
2023-07-07 17:53:30 +0800

    feat: implemented report model for typesafe

---
+++ src/domain/models/ReportModel.ts
@@ -0,0 +1,12 @@
+export type Report = {
+  id: number;
+  name: string;
+  description: string;
+  plateNumber: string;
+  latitude: number;
+  longitude: number;
+  status: number;
+  cancelledAt: string;
+  createdAt: string;
+  updatedAt: string;
+};
