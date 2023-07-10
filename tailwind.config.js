2e7a8d73cc68ae9a0861b03e669cf830fe530a3e
BoJoNVi <bonjomontes@gmail.com>
2023-07-10 09:20:44 +0800

    build: added primary colors for the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary Colors
+        p50: "#e8f4ff",
+        p100: "#d6eaff",
+        p200: "#b5d8ff",
+        p300: "#88bcff",
+        p400: "#5a92ff",
+        p500: "#3468ff",
+        p600: "#1239ff",
+        p700: "#082efa",
+        p800: "#0b2eda",
+        p900: "#132e9c",
+        p950: "#0b185b",
+      },
+    },
   },
   plugins: [],
 };
