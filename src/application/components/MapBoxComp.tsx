73cc7dbeffa12be5887c6a36f820a5eff46b7bc2
BoJoNVi <bonjomontes@gmail.com>
2023-07-11 17:08:48 +0800

    improv: added a default value for the initial coordinations for the mapboxcomp

--- src/application/components/MapBoxComp.tsx
+++ src/application/components/MapBoxComp.tsx
@@ -6,6 +6,11 @@ type Props = React.ComponentProps<typeof Map>;
 function MapBoxComp({ children, ...props }: Props) {
   return (
     <Map
+      initialViewState={{
+        longitude: 125.60355,
+        latitude: 7.077729,
+        zoom: 13,
+      }}
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
       mapStyle={"mapbox://styles/mapbox/streets-v12"}
       {...props}
