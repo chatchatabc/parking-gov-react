107d231a0cb209cfd21d2e19a35f279fdc4cf4ef
BoJoNVi <bonjomontes@gmail.com>
2023-07-11 11:51:48 +0800

    feat: added a new nav for the reports page in the sidebar

--- src/application/components/Sidebar.tsx
+++ src/application/components/Sidebar.tsx
@@ -1,10 +1,31 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import ReportIcon from "../assets/ReportIcon";
+import HomeIcon from "../assets/HomeIcon";
 
 type Props = {
   openSidebar: boolean;
 };
 
+const items = [
+  {
+    name: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    children: [],
+  },
+  {
+    name: "Reports",
+    path: "/reports",
+    icon: <ReportIcon />,
+    children: [],
+  },
+];
+
 function Sidebar({ openSidebar }: Props) {
+  const location = useLocation();
+  const startPath = "/" + location.pathname.split("/")[1];
+  const navigate = useNavigate();
+
   return (
     <aside
       data-sidebar
@@ -13,22 +34,33 @@ function Sidebar({ openSidebar }: Props) {
       } duration-500 border-r border-gray-400 bg-slate-50`}
     >
       <ul className="p-1 text-gray-600">
-        <li className="p-1">
-          <button className="border border-p400 text-p500 bg-p50 p-2 block w-full rounded-md text-start">
-            <div className={`flex items-center relative`}>
-              <div className="w-6 h-6">
-                <ReportIcon />
-              </div>
-              <p
-                className={`left-8 origin-left ${
-                  openSidebar ? "delay-150" : "scale-x-0"
-                } absolute transition overflow-hidden`}
+        {items.map((item) => {
+          return (
+            <li className="p-1">
+              <button
+                onClick={() => {
+                  navigate(item.path);
+                }}
+                className={`p-2 block w-full ${
+                  startPath === item.path
+                    ? "border border-p400 text-p500 bg-p50"
+                    : "border border-slate-50"
+                } rounded-md text-start`}
               >
-                Reports
-              </p>
-            </div>
-          </button>
-        </li>
+                <div className={`flex items-center relative`}>
+                  <div className="w-6 h-6">{item.icon}</div>
+                  <p
+                    className={`left-8 origin-left ${
+                      openSidebar ? "delay-150" : "scale-x-0"
+                    } absolute transition overflow-hidden`}
+                  >
+                    {item.name}
+                  </p>
+                </div>
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
