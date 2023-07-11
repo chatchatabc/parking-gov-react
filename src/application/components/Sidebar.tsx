6265f8ac798161d15491300497d1ad785a65a730
BoJoNVi <bonjomontes@gmail.com>
2023-07-11 10:38:26 +0800

    feat: added a sidebar item

--- src/application/components/Sidebar.tsx
+++ src/application/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import ReportIcon from "../assets/ReportIcon";
+
 type Props = {
   openSidebar: boolean;
 };
@@ -10,7 +12,24 @@ function Sidebar({ openSidebar }: Props) {
         openSidebar ? "min-w-[250px]" : "min-w-[0px]"
       } duration-500 border-r border-gray-400 bg-slate-50`}
     >
-      Sidebar
+      <ul className="p-1 text-gray-600">
+        <li className="p-1">
+          <button className="border border-p400 text-p500 bg-p50 p-2 block w-full rounded-md text-start">
+            <div className={`flex items-center relative`}>
+              <div className="w-6 h-6">
+                <ReportIcon />
+              </div>
+              <p
+                className={`left-8 origin-left ${
+                  openSidebar ? "delay-150" : "scale-x-0"
+                } absolute transition overflow-hidden`}
+              >
+                Reports
+              </p>
+            </div>
+          </button>
+        </li>
+      </ul>
     </aside>
   );
 }
