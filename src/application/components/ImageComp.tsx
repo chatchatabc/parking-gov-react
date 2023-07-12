74a963ba88234454693f3da161527b2c56ea6792
BoJoNVi <bonjomontes@gmail.com>
2023-07-12 10:30:47 +0800

    improv: added an error fallback for the image comp

--- src/application/components/ImageComp.tsx
+++ src/application/components/ImageComp.tsx
@@ -1,10 +1,18 @@
 type Props = {
   src: string;
   alt?: string;
+  className: string;
 };
 
 function ImageComp(props: Props) {
-  return <img className="w-full h-full object-cover" {...props} />;
+  return (
+    <img
+      onError={(e) => {
+        e.currentTarget.src = "/images/lto.png";
+      }}
+      {...props}
+    />
+  );
 }
 
 export default ImageComp;
