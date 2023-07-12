9c353c33936dc474e53c6ecb3ba52453b74fe6d6
BoJoNVi <bonjomontes@gmail.com>
2023-07-12 17:01:44 +0800

    feat: added active key for the dropdown table headers

--- src/application/components/tables/CustomHeaderTable.tsx
+++ src/application/components/tables/CustomHeaderTable.tsx
@@ -112,6 +112,7 @@ function CustomHeaderTable({
           <Dropdown
             trigger={["click"]}
             menu={{
+              activeKey: filter1,
               items: filterOptions,
               onClick: (value) => {
                 setFilters((prev) => {
